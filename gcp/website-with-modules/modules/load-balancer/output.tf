d4c2a221ce1dd8742cc470a879003b888ffd40f0
doug <drehnstrom@yahoo.com>
2021-01-02 19:43:52 -0500

    Changes

---
+++ gcp/website-with-modules/modules/load-balancer/output.tf
@@ -0,0 +1,4 @@
+output "load_balancer_ip" {
+  value = google_compute_global_forwarding_rule.forwarding-rule.ip_address
+  description = "Load balancer IP Address"
+}
---
+++ gcp/website-with-modules/space-invaders-website/output.tf
@@ -0,0 +1,4 @@
+output "load_balancer_ip" {
+  value = module.load-balancer.load_balancer_ip
+  description = "Load balancer IP Address"
+}
