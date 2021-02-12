3701ab90831941c3ba1fb2e7ac05e1d1bc8a8df8
Nathan <beachcoder@outlook.com>
2021-02-12 05:59:36 -0500

    changing vpc lab

--- azure/vpc/custom-vpc.tf
+++ azure/vpc/custom-vpc.tf
@@ -1,10 +1,10 @@
 resource "random_string" "gen-str" {
-  length = 12
+  length = 9
   special = false
   upper = false
   lower = true
   number = true
-  min_numeric = 5
+  min_numeric = 4
 }
 
 resource "azurerm_resource_group" "tf-az-rg" {
@@ -20,6 +20,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.tf-az-rg.name
   tags = var.tags
 }
+
 resource "azurerm_subnet" "subnet0" {
   name = "${var.prefix}-${random_string.gen-str.result}-tf-subnet"
   address_prefixes = [ "10.0.1.0/24" ]
@@ -46,57 +47,20 @@ resource "azurerm_network_security_group" "nsg" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = "*"
+    source_port_range          = "22"
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-}
-
-resource "azurerm_network_interface" "nic" {
-  count = var.instance_count
-  name = "${var.prefix}-vm-${count.index}-nic"
-  location = azurerm_resource_group.tf-az-rg.location
-  resource_group_name = azurerm_resource_group.terra_ref_rg.name
-  tags = var.tags
-
-  ip_configuration {
-    name = "${var.prefix}-vm-nic-cfg"
-    subnet_id = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
-  }
-}
-
-resource "azurerm_virtual_machine" "vm" {
-  count = var.instance_count
-  name = "${var.prefix}-az-tf-server-${count.index}-vm"
-  location = azurerm_resource_group.tf-az-rg.location
-  resource_group_name = azurerm_resource_group.terra_ref_rg.name
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
-  vm_size = var.az_vm_size
-  tags = var.tags
-
-  storage_os_disk {
-    name = "${var.prefix}-tf-os-disk-${count.index}"
-    caching = "ReadWrite"
-    create_option = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = lookup(var.sku,var.res_grp_locale)
-    version = "latest"
-  }
-
-  os_profile {
-    computer_name = "${var.prefix}-az-tf-vm"
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
--- azure/vpc/output.tf
+++ azure/vpc/output.tf
@@ -1,19 +1,14 @@
 output "resource_group_created" {
-  value       = azurerm_resource_group.terra_ref_rg.name
+  value       = azurerm_resource_group.tf-az-rg.name
   description = "Resource group created`"
 }
 
 output "resource_group_created_location" {
-  value       = azurerm_resource_group.terra_ref_rg.location
+  value       = azurerm_resource_group.tf-az-rg.location
   description = "Resource group created location"
 }
 
 output "resource_group_created_id" {
-  value       = azurerm_resource_group.terra_ref_rg.id
+  value       = azurerm_resource_group.tf-az-rg.id
   description = "Resource group created location"
-}
-
-output "vm_instances" {
-  value = [for instance in azurerm_virtual_machine.vm : instance.name]
-  description = "list of vm instances created"
 }
