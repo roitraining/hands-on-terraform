eb7df168c65d2e5c176267bb6354f263a59cf21d
drehnstrom <drehnstrom@yahoo.com>
2022-10-28 10:17:10 -0400

    Updated Azure Examples

--- azure/virtual-machines/firewall.tf
+++ azure/virtual-machines/firewall.tf
@@ -1,51 +1,53 @@
 resource "azurerm_subnet" "AzureFirewallSubnet" {
-  name = "AzureFirewallSubnet" # mandatory name -do not rename-
-  address_prefixes = ["10.0.2.0/26"]
+  name                 = "AzureFirewallSubnet" # mandatory name -do not rename-
+  address_prefixes     = ["10.0.2.0/26"]
   virtual_network_name = azurerm_virtual_network.vpc.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 # Create the public ip for Azure Firewall
 resource "azurerm_public_ip" "azure_firewall_ip" {
-  name = "firewall-ip"
+  name                = "firewall-ip"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  allocation_method = "Static"
-  sku = "Standard"
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 # Create the Azure Firewall
 resource "azurerm_firewall" "firewall" {
-  depends_on=[azurerm_public_ip.azure_firewall_ip]
-  name = "${var.project}-firewall"
+  depends_on          = [azurerm_public_ip.azure_firewall_ip]
+  name                = "${var.project}-firewall"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
   ip_configuration {
-    name = "${var.location}-azure-firewall-config"
-    subnet_id = azurerm_subnet.AzureFirewallSubnet.id
+    name                 = "${var.location}-azure-firewall-config"
+    subnet_id            = azurerm_subnet.AzureFirewallSubnet.id
     public_ip_address_id = azurerm_public_ip.azure_firewall_ip.id
   }
 }
 
 # Create a Azure Firewall Network Rule for Web access
 resource "azurerm_firewall_network_rule_collection" "allow-web" {
-  name = "azure-firewall-web-rule"
+  name                = "azure-firewall-web-rule"
   azure_firewall_name = azurerm_firewall.firewall.name
   resource_group_name = azurerm_resource_group.rg.name
-  priority = 101
-  action = "Allow"
+  priority            = 101
+  action              = "Allow"
   rule {
-    name = "HTTP"
-    source_addresses = ["0.0.0.0/0"]
-    destination_ports = ["80"]
+    name                  = "HTTP"
+    source_addresses      = ["0.0.0.0/0"]
+    destination_ports     = ["80"]
     destination_addresses = ["*"]
-    protocols = ["TCP"]  
+    protocols             = ["TCP"]
   }
   rule {
-    name = "HTTPS"
-    source_addresses = ["0.0.0.0/0"]
-    destination_ports = ["443"]
+    name                  = "HTTPS"
+    source_addresses      = ["0.0.0.0/0"]
+    destination_ports     = ["443"]
     destination_addresses = ["*"]
-    protocols = ["TCP"]
+    protocols             = ["TCP"]
   }
 }
--- azure/website/load-balancer.tf
+++ azure/website/load-balancer.tf
@@ -30,7 +30,7 @@ resource "azurerm_lb_rule" "lbnatrule" {
   protocol                       = "Tcp"
   frontend_port                  = var.application_port
   backend_port                   = var.application_port
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = azurerm_lb_probe.probe.id
 }
