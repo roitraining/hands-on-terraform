141f8f37cae7ac4f76a0413d31f4c517059948ff
stevelockwood <steve@lochnetsystems.com>
2025-08-01 19:49:35 +0000

    Updated gcp-cloud-sql example

--- gcp/cloud-sql/db-server.tf
+++ gcp/cloud-sql/db-server.tf
@@ -1,6 +1,6 @@
 resource "google_sql_database_instance" "my-database" {
   name                = "db-server"
-  database_version    = "MYSQL_5_6"
+  database_version    = "MYSQL_8_0"
   region              = "us-central1"
   deletion_protection = false
 
