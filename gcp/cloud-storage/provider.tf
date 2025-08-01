4f520f9817dc7c674b0161aae14ecdd37b5f7144
stevelockwood <steve@lochnetsystems.com>
2025-08-01 19:51:50 +0000

    Updated gcp-cloud-storage example

--- gcp/cloud-storage/provider.tf
+++ gcp/cloud-storage/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.41.0"
+      version = "6.46.0"
     }
   }
 }
 provider "google" {
-  credentials = file("~/terraform-key.json")
 
   project = var.project_id
   region  = var.region
