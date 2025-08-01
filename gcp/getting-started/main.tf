c848a08c6b43bbc79db7b5ccde56bb22acc6d6c3
stevelockwood <steve@lochnetsystems.com>
2025-08-01 15:04:35 +0000

    minor updates for versions, etc.

--- gcp/getting-started/main.tf
+++ gcp/getting-started/main.tf
@@ -1,11 +1,11 @@
 resource "google_compute_instance" "vm" {
   name         = "${var.project_id}-server"
-  machine_type = "f1-micro"
+  machine_type = "e2-micro"
   zone = var.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "debian-cloud/debian-12"
     }
   }
 
