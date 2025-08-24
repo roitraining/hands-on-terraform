28a2b9da35685b5221f6c7fa08f20fb388f3e1dd
drehnstrom <drehnstrom@yahoo.com>
2025-08-24 08:39:52 -0400

    Updated AWS S3

--- aws/s3-bucket/bucket.tf
+++ aws/s3-bucket/bucket.tf
@@ -1,20 +1,29 @@
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "roi-"
-  
   tags = {
     Name        = "My bucket"
     Environment = "Prod"
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
+# Make the default explicit (ACLs disabled)
+resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+# Block all public access (good hygiene)
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+  versioning_configuration { status = "Enabled" }
 }
