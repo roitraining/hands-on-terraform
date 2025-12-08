81392e162791e8cf2db1374140453e25a0388eac
John Hales <68126264+JohnHales@users.noreply.github.com>
2025-12-08 13:37:11 -0500

    Update cluster.tf to Latest EKS version and node version AMI

--- kubernetes/eks/cluster.tf
+++ kubernetes/eks/cluster.tf
@@ -46,7 +46,7 @@ module "eks" {
   version = "20.8.5"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.34"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
@@ -61,7 +61,7 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type = "AL2023_x86_64_STANDARD"
 
   }
 
