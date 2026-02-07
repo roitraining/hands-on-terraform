b3b848577bb7e4d5de101ae32c6172b309156dca
John Hales <68126264+JohnHales@users.noreply.github.com>
2026-02-07 00:53:33 +0000

    Added encryption to the S3 bucket. Removed roi and changed it to class to be more agnostic.

--- aws/s3-bucket/bucket.tf
+++ aws/s3-bucket/bucket.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket_prefix = "roi-"
+  bucket_prefix = "class-"
   tags = {
     Name        = "My bucket"
     Environment = "Prod"
@@ -26,4 +26,14 @@ resource "aws_s3_bucket_public_access_block" "this" {
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration { status = "Enabled" }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
